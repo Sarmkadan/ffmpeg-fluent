09e4f27c9a0f1846d98895f81742fb92dd9df168
Your Name <you@example.com>
2022-08-09 14:10:36 +0300

    cleanup

--- src/MediaInfo.cs
+++ src/MediaInfo.cs
@@ -7,17 +7,59 @@ using System.Threading.Tasks;
 
 namespace FFmpegFluent
 {
+    /// <summary>
+    /// Represents information about a media file.
+    /// </summary>
     public class MediaInfo
     {
+        /// <summary>
+        /// Gets the duration of the media file.
+        /// </summary>
         public TimeSpan Duration { get; }
+
+        /// <summary>
+        /// Gets the bit rate of the media file.
+        /// </summary>
         public long BitRate { get; }
+
+        /// <summary>
+        /// Gets the video codec of the media file, or null if no video codec is present.
+        /// </summary>
         public string? VideoCodec { get; }
+
+        /// <summary>
+        /// Gets the audio codec of the media file, or null if no audio codec is present.
+        /// </summary>
         public string? AudioCodec { get; }
+
+        /// <summary>
+        /// Gets the width of the video in the media file.
+        /// </summary>
         public int Width { get; }
+
+        /// <summary>
+        /// Gets the height of the video in the media file.
+        /// </summary>
         public int Height { get; }
+
+        /// <summary>
+        /// Gets the frame rate of the video in the media file.
+        /// </summary>
         public double FrameRate { get; }
+
+        /// <summary>
+        /// Gets the number of audio channels in the media file.
+        /// </summary>
         public int AudioChannels { get; }
+
+        /// <summary>
+        /// Gets the sample rate of the audio in the media file.
+        /// </summary>
         public int SampleRate { get; }
+
+        /// <summary>
+        /// Gets the format name of the media file.
+        /// </summary>
         public string FormatName { get; }
 
         private MediaInfo(
@@ -44,6 +86,13 @@ namespace FFmpegFluent
             FormatName = formatName;
         }
 
+        /// <summary>
+        /// Probes the media file at the specified path and returns information about it.
+        /// </summary>
+        /// <param name="filePath">The path to the media file to probe.</param>
+        /// <param name="ffprobePath">The path to the ffprobe executable. Defaults to "ffprobe".</param>
+        /// <param name="ct">A cancellation token to cancel the operation.</param>
+        /// <returns>A MediaInfo object containing information about the media file.</returns>
         public static async Task<MediaInfo> ProbeAsync(string filePath, string ffprobePath = "ffprobe", CancellationToken ct = default)
         {
             var startInfo = new ProcessStartInfo
