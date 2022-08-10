2ca7316b633c750e03350e921492fc0aadf3e65f
Your Name <you@example.com>
2022-08-10 06:24:46 +0300

    extension methods on ThumbnailPreset

---
+++ src/ThumbnailPresetExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FFmpegFluent
+{
+    /// <summary>
+    /// Extension methods that add convenient shortcuts for working with <see cref="ThumbnailPreset"/>.
+    /// </summary>
+    public static class ThumbnailPresetExtensions
+    {
+        /// <summary>
+        /// Positions the thumbnail capture at the midpoint of the supplied video duration.
+        /// </summary>
+        /// <param name="preset">The preset to configure.</param>
+        /// <param name="totalDuration">The total duration of the source video.</param>
+        /// <returns>The same <see cref="ThumbnailPreset"/> instance for fluent chaining.</returns>
+        public static ThumbnailPreset AtMidpoint(this ThumbnailPreset preset, TimeSpan totalDuration)
+        {
+            // Calculate half of the total duration.
+            var midpoint = TimeSpan.FromTicks(totalDuration.Ticks / 2);
+            return preset.AtTime(midpoint);
+        }
+
+        /// <summary>
+        /// Sets a square size for the thumbnail (width == height).
+        /// </summary>
+        /// <param name="preset">The preset to configure.</param>
+        /// <param name="size">The desired width and height in pixels.</param>
+        /// <returns>The same <see cref="ThumbnailPreset"/> instance for fluent chaining.</returns>
+        public static ThumbnailPreset WithSquareSize(this ThumbnailPreset preset, int size)
+        {
+            return preset.WithSize(size, size);
+        }
+
+        /// <summary>
+        /// Conditionally enables smart‑select mode.
+        /// </summary>
+        /// <param name="preset">The preset to configure.</param>
+        /// <param name="condition">If <c>true</c>, <see cref="ThumbnailPreset.UseSmartSelect"/> is applied.</param>
+        /// <returns>The same <see cref="ThumbnailPreset"/> instance for fluent chaining.</returns>
+        public static ThumbnailPreset UseSmartSelectIf(this ThumbnailPreset preset, bool condition)
+        {
+            return condition ? preset.UseSmartSelect() : preset;
+        }
+
+        /// <summary>
+        /// Executes the preset and returns a task that completes when the ffmpeg process finishes.
+        /// This is a thin wrapper around <see cref="ThumbnailPreset.RunAsync"/> that makes the call
+        /// slightly more expressive when used in fluent pipelines.
+        /// </summary>
+        /// <param name="preset">The preset to run.</param>
+        /// <param name="ffmpegPath">Path to the ffmpeg executable (defaults to "ffmpeg").</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public static async Task RunAndWaitAsync(this ThumbnailPreset preset, string ffmpegPath = "ffmpeg", CancellationToken cancellationToken = default)
+        {
+            await preset.RunAsync(ffmpegPath, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
