da5a79018b6768a7d68f93cb5191695b8dcb2ca9
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-22 18:16:11 +0300

    feat: add MediaInfoJson json helpers

---
+++ src/MediaInfoJsonExtensions.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FFmpegFluent
+{
+    /// <summary>
+    /// Provides extension methods for serializing and deserializing <see cref="MediaInfo"/> instances to and from JSON.
+    /// </summary>
+    public static class MediaInfoJsonExtensions
+    {
+        private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+
+        /// <summary>
+        /// Serializes the <see cref="MediaInfo"/> instance to a JSON string.
+        /// </summary>
+        /// <param name="value">The media info instance to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+        /// <returns>A JSON string representation of the media info.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        public static string ToJson(this MediaInfo value, bool indented = false)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var options = indented
+                ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+                : _jsonOptions;
+
+            return JsonSerializer.Serialize(value, options);
+        }
+
+        /// <summary>
+        /// Deserializes a JSON string to a <see cref="MediaInfo"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>The deserialized media info instance, or null if the JSON is null or empty.</returns>
+        /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+        public static MediaInfo? FromJson(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<MediaInfo>(json, _jsonOptions);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a JSON string to a <see cref="MediaInfo"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">Receives the deserialized media info instance if successful.</param>
+        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+        public static bool TryFromJson(string json, out MediaInfo? value)
+        {
+            value = null;
+
+            if (string.IsNullOrEmpty(json))
+            {
+                return false;
+            }
+
+            try
+            {
+                value = JsonSerializer.Deserialize<MediaInfo>(json, _jsonOptions);
+                return true;
+            }
+            catch (JsonException)
+            {
+                return false;
+            }
+        }
+    }
+}
