5fb276bdbdf96115d48c852e16cc5c9ec7894eef
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-29 17:49:30 +0300

    json serialization for FFmpegExceptionJson

--- src/FFmpegExceptionJsonExtensions.cs
+++ src/FFmpegExceptionJsonExtensions.cs
@@ -5,15 +5,14 @@ using System.Text.Json.Serialization;
 namespace FFmpegFluent
 {
     /// <summary>
-    /// JSON serialization helpers for <see cref="FFmpegException"/>.
+    /// Provides JSON serialization and deserialization for <see cref="FFmpegException"/> instances.
     /// </summary>
     public static class FFmpegExceptionJsonExtensions
     {
         // Cached serializer options with camelCase naming policy.
-        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        private static readonly JsonSerializerOptions _options = new()
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            // Preserve case for enum values if any; can be adjusted as needed.
             Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
         };
 
@@ -23,24 +22,19 @@ namespace FFmpegFluent
         /// <param name="value">The exception instance to serialize.</param>
         /// <param name="indented">Whether to format the JSON with indentation.</param>
         /// <returns>A JSON representation of the exception.</returns>
-        public static string ToJson(this FFmpegException value, bool indented = false)
-        {
-            if (value is null)
-                throw new ArgumentNullException(nameof(value));
-
-            var options = indented
-                ? new JsonSerializerOptions(_options) { WriteIndented = true }
-                : _options;
-
-            return JsonSerializer.Serialize(value, options);
-        }
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
+        public static string ToJson(this FFmpegException value, bool indented = false) =>
+            value is null
+                ? throw new ArgumentNullException(nameof(value))
+                : JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_options) { WriteIndented = true } : _options);
 
         /// <summary>
         /// Deserializes a JSON string into an <see cref="FFmpegException"/> instance.
         /// </summary>
         /// <param name="json">The JSON string.</param>
-        /// <returns>The deserialized exception, or <c>null</c> if the JSON is <c>null</c> or empty.</returns>
-        public static FFmpegException? FromJson(string json)
+        /// <returns>The deserialized exception, or <c>null</c> if the JSON is <c>null</c>, empty, or whitespace.</returns>
+        /// <exception cref="JsonException">Thrown when JSON is invalid or cannot be deserialized.</exception>
+        public static FFmpegException? FromJson(string? json)
         {
             if (string.IsNullOrWhiteSpace(json))
                 return null;
@@ -54,7 +48,7 @@ namespace FFmpegFluent
         /// <param name="json">The JSON string.</param>
         /// <param name="value">When this method returns, contains the deserialized exception if the operation succeeded; otherwise, <c>null</c>.</param>
         /// <returns><c>true</c> if deserialization succeeded; otherwise, <c>false</c>.</returns>
-        public static bool TryFromJson(string json, out FFmpegException? value)
+        public static bool TryFromJson(string? json, out FFmpegException? value)
         {
             try
             {
