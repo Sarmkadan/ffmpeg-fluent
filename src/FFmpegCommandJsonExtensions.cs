41d20046de8c3e0eb56f24fb45ef40be72d821b0
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-29 10:14:03 +0300

    feat: cleanup

--- src/FFmpegCommandJsonExtensions.cs
+++ src/FFmpegCommandJsonExtensions.cs
@@ -4,23 +4,47 @@ using System.Text.Json.Serialization;
 
 namespace FFmpegFluent
 {
+    /// <summary>
+    /// Provides JSON serialization and deserialization extensions for <see cref="FFmpegCommand"/> objects.
+    /// </summary>
     public static class FFmpegCommandJsonExtensions
     {
-        private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
 
+        /// <summary>
+        /// Serializes an <see cref="FFmpegCommand"/> instance to a JSON string.
+        /// </summary>
+        /// <param name="value">The FFmpeg command to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+        /// <returns>A JSON string representation of the FFmpeg command.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
         public static string ToJson(this FFmpegCommand value, bool indented = false)
         {
-            if (value == null)
-            {
-                throw new ArgumentNullException(nameof(value));
-            }
+            ArgumentNullException.ThrowIfNull(value);
 
-            var options = indented ? _jsonSerializerOptions : _jsonSerializerOptions;
+            var options = new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = indented
+            };
+
             return JsonSerializer.Serialize(value, options);
         }
 
+        /// <summary>
+        /// Deserializes a JSON string to an <see cref="FFmpegCommand"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>The deserialized FFmpegCommand, or null if deserialization fails.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
         public static FFmpegCommand? FromJson(string json)
         {
+            ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+
             try
             {
                 return JsonSerializer.Deserialize<FFmpegCommand>(json, _jsonSerializerOptions);
@@ -31,8 +55,17 @@ namespace FFmpegFluent
             }
         }
 
+        /// <summary>
+        /// Attempts to deserialize a JSON string to an <see cref="FFmpegCommand"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">Receives the deserialized FFmpegCommand if successful.</param>
+        /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
         public static bool TryFromJson(string json, out FFmpegCommand? value)
         {
+            ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+
             try
             {
                 value = JsonSerializer.Deserialize<FFmpegCommand>(json, _jsonSerializerOptions);
