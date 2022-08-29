8de10736912ccfdb1404487d4e10bb75f070d3fb
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-29 14:58:58 +0300

    add InputFileJson json helpers

--- src/InputFileJsonExtensions.cs
+++ src/InputFileJsonExtensions.cs
@@ -3,43 +3,73 @@ using System.Text.Json.Serialization;
 
 namespace FFmpegFluent;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="InputFile"/> instances.
+/// </summary>
 public static class InputFileJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+		Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+	};
 
-    public static string ToJson(this InputFile value, bool indented = false)
-    {
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+	/// <summary>
+	/// Serializes an <see cref="InputFile"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The input file to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the input file.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	public static string ToJson(this InputFile value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions)
+			{
+				WriteIndented = true
+			}
+			: _jsonOptions;
 
-    public static InputFile? FromJson(string json)
-    {
-        return JsonSerializer.Deserialize<InputFile>(json, _jsonOptions);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    public static bool TryFromJson(string json, out InputFile? value)
-    {
-        try
-        {
-            value = JsonSerializer.Deserialize<InputFile>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+	/// <summary>
+	/// Deserializes an <see cref="InputFile"/> instance from a JSON string.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized <see cref="InputFile"/> instance, or null if deserialization fails.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	public static InputFile? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+
+		return JsonSerializer.Deserialize<InputFile>(json, _jsonOptions);
+	}
+
+	/// <summary>
+	/// Attempts to deserialize an <see cref="InputFile"/> instance from a JSON string.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized instance if successful.</param>
+	/// <returns>True if deserialization succeeded; otherwise, false.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	public static bool TryFromJson(string json, out InputFile? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+
+		try
+		{
+			value = JsonSerializer.Deserialize<InputFile>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
