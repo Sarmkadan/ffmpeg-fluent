9442617bad23857f9a9890d04ed910b4889be9c4
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-29 07:59:26 +0300

    reshuffle WatermarkHelepr a bit

--- src/WatermarkHelper.cs
+++ src/WatermarkHelper.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace FFmpegFluent;
 
+/// <summary>
+/// Defines the position where the watermark will be placed on the video.
+/// </summary>
 public enum WatermarkPosition
 {
     TopLeft,
@@ -13,6 +16,10 @@ public enum WatermarkPosition
     Center
 }
 
+/// <summary>
+/// Helper class for adding watermarks to video files using FFmpeg.
+/// Provides fluent API for configuring watermark position, opacity, scale, and executing the watermarking process.
+/// </summary>
 public sealed class WatermarkHelper
 {
     private readonly string _inputPath;
@@ -23,21 +30,40 @@ public sealed class WatermarkHelper
     private double _opacity = 0.5;
     private double _scale = 0.2;
 
-    public WatermarkHelper(string inputPath, string watermarkPath, string outputPath)
+    /// <summary>
+/// Initializes a new instance of the <see cref="WatermarkHelper"/> class.
+/// </summary>
+/// <param name="inputPath">The path to the input video file.</param>
+/// <param name="watermarkPath">The path to the watermark image file.</param>
+/// <param name="outputPath">The path where the output video with watermark will be saved.</param>
+/// <exception cref="ArgumentNullException">Thrown when any of the paths is null.</exception>
+public WatermarkHelper(string inputPath, string watermarkPath, string outputPath)
     {
         _inputPath = inputPath ?? throw new ArgumentNullException(nameof(inputPath));
         _watermarkPath = watermarkPath ?? throw new ArgumentNullException(nameof(watermarkPath));
         _outputPath = outputPath ?? throw new ArgumentNullException(nameof(outputPath));
     }
 
-    public WatermarkHelper At(WatermarkPosition pos, int margin = 10)
+    /// <summary>
+/// Sets the position and margin for the watermark.
+/// </summary>
+/// <param name="pos">The position where the watermark will be placed on the video.</param>
+/// <param name="margin">The margin in pixels from the edge of the video. Default is 10.</param>
+/// <returns>The current <see cref="WatermarkHelper"/> instance for method chaining.</returns>
+public WatermarkHelper At(WatermarkPosition pos, int margin = 10)
     {
         _position = pos;
         _margin = margin;
         return this;
     }
 
-    public WatermarkHelper WithOpacity(double opacity)
+    /// <summary>
+/// Sets the opacity of the watermark.
+/// </summary>
+/// <param name="opacity">The opacity value between 0 (fully transparent) and 1 (fully opaque).</param>
+/// <returns>The current <see cref="WatermarkHelper"/> instance for method chaining.</returns>
+/// <exception cref="ArgumentOutOfRangeException">Thrown when opacity is not between 0 and 1.</exception>
+public WatermarkHelper WithOpacity(double opacity)
     {
         if (opacity < 0 || opacity > 1)
         {
@@ -47,7 +73,13 @@ public sealed class WatermarkHelper
         return this;
     }
 
-    public WatermarkHelper WithScale(double factor)
+    /// <summary>
+/// Sets the scale factor for the watermark.
+/// </summary>
+/// <param name="factor">The scaling factor for the watermark. Must be a positive value.</param>
+/// <returns>The current <see cref="WatermarkHelper"/> instance for method chaining.</returns>
+/// <exception cref="ArgumentOutOfRangeException">Thrown when factor is not positive.</exception>
+public WatermarkHelper WithScale(double factor)
     {
         if (factor <= 0)
         {
@@ -57,7 +89,11 @@ public sealed class WatermarkHelper
         return this;
     }
 
-    public string BuildFilterComplex()
+    /// <summary>
+/// Builds the FFmpeg filter complex string for applying the watermark.
+/// </summary>
+/// <returns>A filter complex string that can be used with FFmpeg to overlay the watermark on the video.</returns>
+public string BuildFilterComplex()
     {
         // Calculate position parameters based on watermark position
         string positionX, positionY;
@@ -92,7 +128,11 @@ public sealed class WatermarkHelper
         return $"[0:v][1:v]overlay={positionX}:{positionY}:format=auto,format=yuv420p";
     }
 
-    public string[] BuildArguments()
+    /// <summary>
+/// Builds the complete FFmpeg command arguments array for executing the watermarking process.
+/// </summary>
+/// <returns>An array of strings containing all FFmpeg command arguments including input/output paths and filter configuration.</returns>
+public string[] BuildArguments()
     {
         return [
             "-y",
@@ -109,7 +149,14 @@ public sealed class WatermarkHelper
         ];
     }
 
-    public async Task RunAsync(string ffmpegPath = "ffmpeg", CancellationToken ct = default)
+    /// <summary>
+/// Asynchronously executes the FFmpeg process to apply the watermark to the video.
+/// </summary>
+/// <param name="ffmpegPath">The path to the FFmpeg executable. Defaults to "ffmpeg".</param>
+/// <param name="ct">Cancellation token for cancelling the operation.</param>
+/// <returns>A Task representing the asynchronous operation.</returns>
+/// <exception cref="InvalidOperationException">Thrown when the FFmpeg process exits with a non-zero exit code.</exception>
+public async Task RunAsync(string ffmpegPath = "ffmpeg", CancellationToken ct = default)
     {
         var arguments = BuildArguments();
         var process = new System.Diagnostics.Process
