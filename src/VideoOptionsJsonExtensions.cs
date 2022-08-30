b341e4a64b99a36d00d7f69d791ba547c92c5093
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-30 19:33:56 +0300

    feat: serialize VideoOptionsJson properly

--- src/VideoOptionsJsonExtensions.cs
+++ src/VideoOptionsJsonExtensions.cs
@@ -1,5 +1,6 @@
 #nullable enable
 
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -10,7 +11,7 @@ namespace FFmpegFluent;
 /// </summary>
 public static class VideoOptionsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _jsonOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
@@ -23,27 +24,27 @@ public static class VideoOptionsJsonExtensions
     /// <param name="value">The video options to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation.</param>
     /// <returns>A JSON string representation of the video options.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this VideoOptions value, bool indented = false)
     {
-        if (value is null)
-        {
-            return "{}";
-        }
+        ArgumentNullException.ThrowIfNull(value);
 
-        var options = new JsonSerializerOptions(_jsonOptions)
+        return JsonSerializer.Serialize(value, new JsonSerializerOptions(_jsonOptions)
         {
             WriteIndented = indented
-        };
-        return JsonSerializer.Serialize(value, options);
+        });
     }
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="VideoOptions"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A <see cref="VideoOptions"/> instance, or null if deserialization fails.</returns>
+    /// <returns>A <see cref="VideoOptions"/> instance, or <see langword="null"/> if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static VideoOptions? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         try
         {
             return JsonSerializer.Deserialize<VideoOptions>(json, _jsonOptions);
@@ -58,10 +59,13 @@ public static class VideoOptionsJsonExtensions
     /// Attempts to deserialize a JSON string to a <see cref="VideoOptions"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized value, or null if deserialization fails.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <param name="value">Receives the deserialized value, or <see langword="null"/> if deserialization fails.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out VideoOptions? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         try
         {
             value = JsonSerializer.Deserialize<VideoOptions>(json, _jsonOptions);
