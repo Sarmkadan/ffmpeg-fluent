9c0db4f3ddae776207eafefa66bd0e9c59a46f16
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-01 20:52:00 +0300

    feat: json serialization for ThumbnailPresetJson

--- src/ThumbnailPresetJsonExtensions.cs
+++ src/ThumbnailPresetJsonExtensions.cs
@@ -8,56 +8,57 @@ namespace FFmpegFluent
     /// </summary>
     public static class ThumbnailPresetJsonExtensions
     {
-        // Cached options with camel‑case naming. WriteIndented is set per call.
-        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        // Cached options with camel-case naming. WriteIndented is set per call.
+        private static readonly JsonSerializerOptions _options = new()
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             // Preserve defaults for other settings (e.g., ignore null values) as needed.
         };
 
         /// <summary>
-        /// Serialises the <paramref name="value"/> to JSON.
+        /// Serializes the <paramref name="value"/> to JSON.
         /// </summary>
-        /// <param name="value">The <see cref="ThumbnailPreset"/> instance to serialise.</param>
+        /// <param name="value">The <see cref="ThumbnailPreset"/> instance to serialize.</param>
         /// <param name="indented">If <c>true</c>, the output JSON will be indented.</param>
         /// <returns>A JSON string representing the object.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
         public static string ToJson(this ThumbnailPreset value, bool indented = false)
         {
-            if (value is null) throw new ArgumentNullException(nameof(value));
+            ArgumentNullException.ThrowIfNull(value);
 
             // Create a copy of the cached options with the requested indentation setting.
-            var options = new JsonSerializerOptions(_options)
-            {
-                WriteIndented = indented
-            };
+            var options = new JsonSerializerOptions(_options) { WriteIndented = indented };
 
             return JsonSerializer.Serialize(value, options);
         }
 
         /// <summary>
-        /// Deserialises a JSON string into a <see cref="ThumbnailPreset"/> instance.
+        /// Deserializes a JSON string into a <see cref="ThumbnailPreset"/> instance.
         /// </summary>
         /// <param name="json">The JSON representation of a <see cref="ThumbnailPreset"/>.</param>
-        /// <returns>The deserialised <see cref="ThumbnailPreset"/>, or <c>null</c> if the JSON is <c>null</c> or empty.</returns>
+        /// <returns>The deserialized <see cref="ThumbnailPreset"/>, or <c>null</c> if the JSON is <c>null</c> or empty.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
         public static ThumbnailPreset? FromJson(string json)
         {
-            if (string.IsNullOrWhiteSpace(json))
-                return null;
+            ArgumentException.ThrowIfNullOrEmpty(json);
 
             return JsonSerializer.Deserialize<ThumbnailPreset>(json, _options);
         }
 
         /// <summary>
-        /// Tries to deserialise a JSON string into a <see cref="ThumbnailPreset"/> instance.
+        /// Attempts to deserialize a JSON string into a <see cref="ThumbnailPreset"/> instance.
         /// </summary>
         /// <param name="json">The JSON representation of a <see cref="ThumbnailPreset"/>.</param>
         /// <param name="value">When this method returns, contains the deserialized value if the operation succeeded; otherwise <c>null</c>.</param>
-        /// <returns><c>true</c> if deserialization succeeded; otherwise <c>false</c>.</returns>
+        /// <returns><c>true</c> if deserialization succeeds; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
         public static bool TryFromJson(string json, out ThumbnailPreset? value)
         {
+            ArgumentException.ThrowIfNullOrEmpty(json);
+
             try
             {
-                value = FromJson(json);
+                value = JsonSerializer.Deserialize<ThumbnailPreset>(json, _options);
                 return true;
             }
             catch (JsonException)
