2422c923105bb33a0abb95f18c82c0121d7badd3
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-05 06:10:13 +0300

    feat: add WatermarkHelperJson json helpers

--- src/WatermarkHelperJsonExtensions.cs
+++ src/WatermarkHelperJsonExtensions.cs
@@ -4,36 +4,67 @@ using System.Text.Json.Serialization;
 
 namespace FFmpegFluent
 {
+    /// <summary>
+    /// Provides JSON serialization and deserialization extensions for <see cref="WatermarkHelper"/> instances.
+    /// </summary>
     public static class WatermarkHelperJsonExtensions
     {
-        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        /// <summary>
+        /// Gets the default JSON serializer options used for serializing and deserializing <see cref="WatermarkHelper"/> instances.
+        /// Uses camelCase property naming policy and ignores null values.
+        /// </summary>
+        private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
         {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
         };
 
+        /// <summary>
+        /// Serializes a <see cref="WatermarkHelper"/> instance to a JSON string.
+        /// </summary>
+        /// <param name="value">The watermark helper to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+        /// <returns>A JSON string representation of the watermark helper.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
         public static string ToJson(this WatermarkHelper value, bool indented = false)
         {
-            if (indented)
-            {
-                JsonOptions.WriteIndented = true;
-            }
-            else
-            {
-                JsonOptions.WriteIndented = false;
-            }
-            return JsonSerializer.Serialize(value, JsonOptions);
+            ArgumentNullException.ThrowIfNull(value);
+
+            var options = indented
+                ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+                : _jsonOptions;
+
+            return JsonSerializer.Serialize(value, options);
         }
 
+        /// <summary>
+        /// Deserializes a <see cref="WatermarkHelper"/> instance from a JSON string.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>The deserialized <see cref="WatermarkHelper"/> instance, or null if deserialization fails.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
         public static WatermarkHelper? FromJson(string json)
         {
-            return JsonSerializer.Deserialize<WatermarkHelper>(json, JsonOptions);
+            ArgumentNullException.ThrowIfNull(json);
+
+            return JsonSerializer.Deserialize<WatermarkHelper>(json, _jsonOptions);
         }
 
+        /// <summary>
+        /// Attempts to deserialize a <see cref="WatermarkHelper"/> instance from a JSON string.
+        /// </summary>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">Receives the deserialized instance if successful.</param>
+        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
         public static bool TryFromJson(string json, out WatermarkHelper? value)
         {
+            ArgumentNullException.ThrowIfNull(json);
+
             try
             {
-                value = JsonSerializer.Deserialize<WatermarkHelper>(json, JsonOptions);
+                value = JsonSerializer.Deserialize<WatermarkHelper>(json, _jsonOptions);
                 return true;
             }
             catch (JsonException)
