0188b6dc2004da1970ab11c19836630b8119bcc5
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-06 15:03:13 +0300

    stop HardwareAccelOptions blowing up on bad input

--- src/HardwareAccelOptionsValidation.cs
+++ src/HardwareAccelOptionsValidation.cs
@@ -12,15 +12,12 @@ namespace FFmpegFluent
         /// </summary>
         /// <param name="value">The options to validate.</param>
         /// <returns>A list of validation problems (empty if valid).</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
         public static IReadOnlyList<string> Validate(this HardwareAccelOptions? value)
         {
-            var problems = new List<string>();
+            ArgumentNullException.ThrowIfNull(value);
 
-            if (value == null)
-            {
-                problems.Add("HardwareAccelOptions cannot be null.");
-                return problems;
-            }
+            var problems = new List<string>();
 
             // Validate Kind
             if (value.Kind == HwAccelKind.None)
@@ -33,10 +30,9 @@ namespace FFmpegFluent
             {
                 problems.Add("HardwareAccelOptions.Device cannot be null or whitespace when Kind is Vaapi.");
             }
-
-            // Validate Device path format for VAAPI (basic validation)
-            if (value.Kind == HwAccelKind.Vaapi && !string.IsNullOrWhiteSpace(value.Device))
+            else if (value.Kind == HwAccelKind.Vaapi && !string.IsNullOrWhiteSpace(value.Device))
             {
+                // Validate Device path format for VAAPI (basic validation)
                 if (!value.Device.StartsWith("/dev/", StringComparison.Ordinal))
                 {
                     problems.Add("HardwareAccelOptions.Device should be a valid device path starting with '/dev/' when Kind is Vaapi.");
@@ -51,29 +47,24 @@ namespace FFmpegFluent
         /// </summary>
         /// <param name="value">The options to check.</param>
         /// <returns>True if the instance is valid; otherwise, false.</returns>
-        public static bool IsValid(this HardwareAccelOptions? value)
-        {
-            return value != null && Validate(value).Count == 0;
-        }
+        public static bool IsValid(this HardwareAccelOptions? value) => value is not null && Validate(value).Count == 0;
 
         /// <summary>
         /// Ensures that the specified <see cref="HardwareAccelOptions"/> instance is valid.
         /// </summary>
         /// <param name="value">The options to validate.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentException">Thrown when the instance is not valid.</exception>
         public static void EnsureValid(this HardwareAccelOptions? value)
         {
-            if (value == null)
-            {
-                throw new ArgumentException("HardwareAccelOptions cannot be null.");
-            }
+            ArgumentNullException.ThrowIfNull(value);
 
             var problems = Validate(value);
 
             if (problems.Count > 0)
             {
                 throw new ArgumentException(
-                    $"HardwareAccelOptions is not valid. Problems:\n{string.Join("\n", problems)}");
+                    $"HardwareAccelOptions is not valid. Problems:{Environment.NewLine}{string.Join(Environment.NewLine, problems)}");
             }
         }
     }
