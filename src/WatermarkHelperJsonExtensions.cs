b0049287811adf58dcf50a4e18824f1f92bea125
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-08 15:27:34 +0300

    Serialize WatermarkHelperJson properly

--- src/WatermarkHelperJsonExtensions.cs
+++ src/WatermarkHelperJsonExtensions.cs
@@ -21,12 +21,12 @@ namespace FFmpegFluent
         };
 
         /// <summary>
-        /// Serializes a <see cref="WatermarkHelper"/> instance to a JSON string.
+        /// Serializes the specified <see cref="WatermarkHelper"/> instance to a JSON string.
         /// </summary>
         /// <param name="value">The watermark helper to serialize.</param>
         /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
         /// <returns>A JSON string representation of the watermark helper.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
         public static string ToJson(this WatermarkHelper value, bool indented = false)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -39,11 +39,14 @@ namespace FFmpegFluent
         }
 
         /// <summary>
-        /// Deserializes a <see cref="WatermarkHelper"/> instance from a JSON string.
+        /// Deserializes a JSON string to a <see cref="WatermarkHelper"/> instance.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <returns>The deserialized <see cref="WatermarkHelper"/> instance, or null if deserialization fails.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+        /// <returns>
+        /// The deserialized <see cref="WatermarkHelper"/> instance, or <see langword="null"/>
+        /// if deserialization fails due to invalid JSON format.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
         public static WatermarkHelper? FromJson(string json)
         {
             ArgumentNullException.ThrowIfNull(json);
@@ -52,12 +55,14 @@ namespace FFmpegFluent
         }
 
         /// <summary>
-        /// Attempts to deserialize a <see cref="WatermarkHelper"/> instance from a JSON string.
+        /// Attempts to deserialize a JSON string to a <see cref="WatermarkHelper"/> instance.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <param name="value">Receives the deserialized instance if successful.</param>
-        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+        /// <param name="value">Receives the deserialized <see cref="WatermarkHelper"/> instance if successful.</param>
+        /// <returns>
+        /// <see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
         public static bool TryFromJson(string json, out WatermarkHelper? value)
         {
             ArgumentNullException.ThrowIfNull(json);
