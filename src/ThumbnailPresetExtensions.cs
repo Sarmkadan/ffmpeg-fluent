cd842d5788cb7356163f7e4e823204ab6e9b6052
Vladyslav Zaiets <rutova2@gmail.com>
2022-09-08 07:22:28 +0300

    a couple of ThumbnailPreset extensions

--- src/ThumbnailPresetExtensions.cs
+++ src/ThumbnailPresetExtensions.cs
@@ -15,8 +15,11 @@ namespace FFmpegFluent
         /// <param name="preset">The preset to configure.</param>
         /// <param name="totalDuration">The total duration of the source video.</param>
         /// <returns>The same <see cref="ThumbnailPreset"/> instance for fluent chaining.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="preset"/> is <see langword="null"/>.</exception>
         public static ThumbnailPreset AtMidpoint(this ThumbnailPreset preset, TimeSpan totalDuration)
         {
+            ArgumentNullException.ThrowIfNull(preset);
+
             // Calculate half of the total duration.
             var midpoint = TimeSpan.FromTicks(totalDuration.Ticks / 2);
             return preset.AtTime(midpoint);
@@ -28,8 +31,16 @@ namespace FFmpegFluent
         /// <param name="preset">The preset to configure.</param>
         /// <param name="size">The desired width and height in pixels.</param>
         /// <returns>The same <see cref="ThumbnailPreset"/> instance for fluent chaining.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="preset"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="size"/> is zero or negative.</exception>
         public static ThumbnailPreset WithSquareSize(this ThumbnailPreset preset, int size)
         {
+            ArgumentNullException.ThrowIfNull(preset);
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
             return preset.WithSize(size, size);
         }
 
@@ -39,8 +50,10 @@ namespace FFmpegFluent
         /// <param name="preset">The preset to configure.</param>
         /// <param name="condition">If <c>true</c>, <see cref="ThumbnailPreset.UseSmartSelect"/> is applied.</param>
         /// <returns>The same <see cref="ThumbnailPreset"/> instance for fluent chaining.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="preset"/> is <see langword="null"/>.</exception>
         public static ThumbnailPreset UseSmartSelectIf(this ThumbnailPreset preset, bool condition)
         {
+            ArgumentNullException.ThrowIfNull(preset);
             return condition ? preset.UseSmartSelect() : preset;
         }
 
@@ -53,8 +66,16 @@ namespace FFmpegFluent
         /// <param name="ffmpegPath">Path to the ffmpeg executable (defaults to "ffmpeg").</param>
         /// <param name="cancellationToken">Optional cancellation token.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="preset"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="ffmpegPath"/> is null or whitespace.</exception>
         public static async Task RunAndWaitAsync(this ThumbnailPreset preset, string ffmpegPath = "ffmpeg", CancellationToken cancellationToken = default)
         {
+            ArgumentNullException.ThrowIfNull(preset);
+            if (string.IsNullOrWhiteSpace(ffmpegPath))
+            {
+                throw new ArgumentException("ffmpeg path must not be empty.", nameof(ffmpegPath));
+            }
+
             await preset.RunAsync(ffmpegPath, cancellationToken).ConfigureAwait(false);
         }
     }
