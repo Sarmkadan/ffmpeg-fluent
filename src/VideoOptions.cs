603548c34f38b7cfa5d8b57b1448f2bfd067f450
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-21 11:14:35 +0300

    add VideoOptions.Crop(width,height,x,y) with validation

--- src/VideoOptions.cs
+++ src/VideoOptions.cs
@@ -16,6 +16,10 @@ public sealed class VideoOptions
     private double? _frameRate;
     private int? _width;
     private int? _height;
+private int? _cropX;
+private int? _cropY;
+private int? _cropWidth;
+private int? _cropHeight;
     private bool _noVideo;
 
     /// <summary>
@@ -72,6 +76,42 @@ public sealed class VideoOptions
         _height = height;
         return this;
     }
+
+/// <summary>
+/// Crops the video to the specified rectangle. All parameters must be non-negative.
+/// This will be emitted as a crop video filter.
+/// </summary>
+/// <param name="width">Width of the crop rectangle (must be &gt; 0)</param>
+/// <param name="height">Height of the crop rectangle (must be &gt; 0)</param>
+/// <param name="x">X offset of the crop rectangle (must be &gt;= 0)</param>
+/// <param name="y">Y offset of the crop rectangle (must be &gt;= 0)</param>
+/// <returns>The VideoOptions instance for fluent chaining.</returns>
+/// <exception cref="ArgumentOutOfRangeException">Thrown if any parameter is negative.</exception>
+public VideoOptions Crop(int width, int height, int x, int y)
+{
+	if (width < 0)
+	{
+		throw new ArgumentOutOfRangeException(nameof(width), "Crop width must be non-negative");
+	}
+	if (height < 0)
+	{
+		throw new ArgumentOutOfRangeException(nameof(height), "Crop height must be non-negative");
+	}
+	if (x < 0)
+	{
+		throw new ArgumentOutOfRangeException(nameof(x), "Crop x offset must be non-negative");
+	}
+	if (y < 0)
+	{
+		throw new ArgumentOutOfRangeException(nameof(y), "Crop y offset must be non-negative");
+	}
+
+	_cropWidth = width;
+	_cropHeight = height;
+	_cropX = x;
+	_cropY = y;
+	return this;
+}
 
     /// <summary>
     /// Disables video stream in the output.
@@ -131,6 +171,13 @@ public sealed class VideoOptions
             args.Add("-vf");
             args.Add($"scale={_width.Value}:{_height.Value}");
         }
+
+	if (_cropWidth.HasValue && _cropHeight.HasValue)
+	{
+		// Use a crop filter to crop the video.
+		args.Add("-vf");
+		args.Add($"crop={_cropWidth.Value}:{_cropHeight.Value}:{_cropX.Value}:{_cropY.Value}");
+	}
 
         return args;
     }
